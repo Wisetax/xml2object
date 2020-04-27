9799fec60c01afd051c7cf054e43ea6ecd3a2caa
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2020-04-27 18:56:37 +0200

    Code documentation

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const xpath = require('xpath');
 const dom = require('xmldom').DOMParser;
 const fspath = require('path');
 
-
+/**
+ * Extract xml arccording to given mapping
+ * @param  {String} xml xml as string
+ * @param  {Object} mapping mapping define according to documentation
+ */
 function extract(xml, mapping) {
   const doc = new dom().parseFromString(xml);
 
@@ -15,24 +19,36 @@ function extract(xml, mapping) {
   return object;
 
 }
+/**
+ * Extract one single key given xpath and options
+ * @param  {Element} doc doc element as returnes by xmldom
+ * @param  {Object | String} options  options given according to documentation (if string is given the options is converted to  { path: options}
+ * 
+ * @return {Array | Object | String} return object designed by the path (could be a string, an array or object)
+ */
+function extractKey(doc, options) {
 
-function extractKey(doc, path) {
-
-  if (typeof path === 'string')
-    return _extractPlainKey(doc, path);
+  if (typeof options === 'string')
+    options = {path: options}
   
-  if (path.isHtml)
-    return _extractHtmlKey(doc, path.path);
+  if (options.isHtml)
+    return _extractHtmlKey(doc, options.path);
 
-  if (path.mapping)
-    return _extracItemsKey(doc, path);
+  if (options.mapping)
+    return _extracItemsKey(doc, options);
 
-  if (path.path)
-    return _extractPlainKey(doc, path.path);
+  if (options.path)
+    return _extractPlainKey(doc, options.path);
 
   throw new Error('Path format not recognized');
 }
-
+/**
+ * Find node given an xpath
+ * @param  {Element} doc doc element as returnes by xmldom
+ * @param  {String} path xpath
+ * 
+ * @returns {Array{Element}} array of elements
+ */
 function extractNodes(doc, path) {
   const nodes = xpath.select(path, doc);
 
@@ -40,9 +56,48 @@ function extractNodes(doc, path) {
     throw new Error(`No element for this path: ${path}`);
 
   return nodes;
-
 }
+/**
+ * Extract simple key (attr or text)
+ * @param  {Element} doc
+ * @param  {String} path xpath 
+ * 
+ * @return {Array<String> | String} returrn array of extracted texts 
+ */
+function _extractPlainKey(doc, path) {
+  const nodes = extractNodes(doc, path);
+  
+  return _extractValue(nodes, (node) => node.nodeValue)
+}
+/**
+ * Extract html content from key
+ * @param  {Element} doc
+ * @param  {String} path xpath
+ * 
+ * @return {Array<String> | String} return array of html content or single html content in a string
+ */
+function _extractHtmlKey(doc, path) {
+  const nodes = extractNodes(doc, path);
 
+  return _extractValue(nodes, (node) => {
+    const stripTagStart = new RegExp(`^<${node.tagName}[^>]*>`);
+    const stripTagEnd = new RegExp(`</${node.tagName}>$`);
+
+    let innerHtml = node.toString(true);
+    innerHtml = innerHtml.replace(stripTagStart, '')
+    innerHtml = innerHtml.replace(stripTagEnd, '')
+    
+    return innerHtml;
+  });
+}
+/**
+ * Extract nested objects (reccursive)
+ * @param  {Element} doc
+ * @param  {String} {path xpath
+ * @param  {Object} mapping} mapping for nested objects (relative)
+ * 
+ * @return {Array | Object | String}
+ */
 function _extracItemsKey(doc, {path, mapping}) {
   const nodes = extractNodes(doc, path);
 
@@ -70,28 +125,11 @@ function _extracItemsKey(doc, {path, mapping}) {
   })
 }
 
-function _extractHtmlKey(doc, path) {
-  // path = fspath.join(path, '*');
-  const nodes = extractNodes(doc, path);
-
-  return _extractValue(nodes, (node) => {
-    const stripTagStart = new RegExp(`^<${node.tagName}[^>]*>`);
-    const stripTagEnd = new RegExp(`</${node.tagName}>$`);
-
-    let innerHtml = node.toString(true);
-    innerHtml = innerHtml.replace(stripTagStart, '')
-    innerHtml = innerHtml.replace(stripTagEnd, '')
-    
-    return innerHtml;
-  });
-}
-
-function _extractPlainKey(doc, path) {
-  const nodes = extractNodes(doc, path);
-  
-  return _extractValue(nodes, (node) => node.nodeValue)
-}
-
+/**
+ * Apply function to one or multiple nodes
+ * @param  {Array[Element]} nodes
+ * @param  {Function} func function to apply to the node to retrive the value
+ */
 function _extractValue(nodes, func) {
   
   if (nodes.length === 1)
